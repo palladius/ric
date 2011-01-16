5e00756bacdaffc6c7a4bdcb5204e1060967d366
Riccardo Carlesso <rusko@palladius.it>
2011-01-16 15:37:54 +0000

    a few tests..

---
+++ lib/riclib.rb
@@ -0,0 +1,19 @@
+
+# RICLIB!
+
+module RicLib
+  def self.say_hello
+    puts 'hello world'
+  end
+
+  def red(str)
+  	 "Riclib::Red "
+  end
+  
+  def help
+    puts "This is RicLib Riccardo library (my firdst gem!)"
+  end
+  
+end
+
+
